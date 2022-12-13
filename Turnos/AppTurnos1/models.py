c4532a8293dbd818e2c919168fcc88be915c34e1
desimonewalter1971 <desimonewalter71@gmail.com>
2022-12-13 20:03:50 -0300

    Agenda time

--- Turnos/AppTurnos1/models.py
+++ Turnos/AppTurnos1/models.py
@@ -28,9 +28,8 @@ class pacientes(models.Model):
 
 class agendaDisponiblePorMedico(models.Model):
     medico= models.CharField(max_length=40)
-    dia = models.DateField()
-    desde = models.DateTimeField('dia/hora', null=True ,  blank=True)
-    hasta = models.TimeField('tiempo', null=True,  blank=True)
+    desde = models.DateTimeField('dia/desde hora', null=True ,  blank=True)
+    hasta = models.TimeField('hora hast', null=True,  blank=True)
 
 class turnoReservado(models.Model):
     DNI=models.IntegerField()
